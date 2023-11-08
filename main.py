4493abbcf8edec5ba50d336e53f282c21ce7f31b
Pawan Mehra <pawan.mehra@intrics.in>
2023-11-09 01:31:14 +0530

    working code, requirements, 2 batch

---
+++ main.py
@@ -0,0 +1,24 @@
+from jinja2 import Environment, FileSystemLoader
+import pdfkit
+
+# Define your HTML template
+templateLoader = FileSystemLoader(searchpath="./")
+env = Environment(loader=templateLoader)
+template = env.get_template("templates/index.html")
+data = {
+    'BETHESDA': '20.67 %',
+    'EVERETT': '18.32 %',
+    'GREAT LAKES': '18.92 %',
+    'JACKSONVILLE': '18.01 %'
+}
+
+# Render the template with some sample data
+rendered = template.render(data=data,total_saving='19.44')
+
+# Configuration for pdfkit
+config = pdfkit.configuration(wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe')
+
+# Generate PDF from the rendered HTML
+pdfkit.from_string(rendered, "Output.pdf", configuration=config)
+
+print("PDF generated")
